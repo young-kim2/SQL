cb3625d5f778980a357de5b53189e8398118b678
김영 <dhehd215@daum.net>
2021-07-17 11:43:06 +0900

    Practice04 Update

--- practice04.sql
+++ practice04.sql
@@ -138,10 +138,23 @@ FROM employees emp,(SELECT department_id,
 AVG(salary) salary
 FROM employees
 GROUP BY department_id)t
-WHERE emp.department_id=t.department_id;
+WHERE emp.department_id=t.department_id
+AND emp.salary > t.salary;
 
 /*
 문제8
 직원 입사일이 11번째에서 15번째의 직원의 사번,
 이름, 급여, 입사일을 입사일 순서로 출력하세요
 */
+--입사일 순 정렬 쿼리
+SELECT employee_id, first_name, salary, hire_date
+FROM employees ORDER BY hire_date;
+SELECT rownum rn, employee_id, first_name, salary, hire_date
+FROM (SELECT employee_id, first_name, salary, hire_date
+FROM employees ORDER BY hire_date);
+
+SELECT rn, employee_id, first_name, salary, hire_date 
+FROM (SELECT rownum rn, employee_id, first_name,salary, hire_date
+FROM (SELECT employee_id, first_name, salary, hire_date FROM employees ORDER BY hire_date)
+)
+WHERE rn BETWEEN 11 AND 15;
